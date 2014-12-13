31517c081401ce0ef857f583e0210bef961d8ac0
Jackson Hoose <jacksonhoose@gmail.com>
2014-12-12 19:08:17 -0800

    finished binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -26,24 +26,38 @@ var BinarySearchTree = function(value){
 	tree.contains = function(val, parent){
 		parent = parent || this;
 
-		// return _.reduce(tree, function(val, tree){
-		// 	return t
-		// }, tree.value);
 		if(parent.value === val) {
 			return true;
-		}
-		if(parent.left === null && parent.right === null){
-			return false;
+		} 
+
+		if(val < parent.value){
+			if(parent.left !== null){
+				if(parent.left.contains(val, parent.left)) 
+					return true;
+			} 
 		} else {
-			if(val < parent.value){
-				parent.contains(val, parent.left);
-			} else {
-				parent.contains(val, parent.right);
+			if(parent.right !== null){
+				if(parent.right.contains(val, parent.right)) 
+					return true;
 			}
 		}
+
+		return false;
+
 	};
 
-	tree.depthFirstLog = function(){
+	tree.depthFirstLog = function(cb, parent){
+		parent = parent || this;
+		
+		cb(parent.value);
+
+		if(parent.right){
+			parent.right.depthFirstLog(cb, parent.right);
+		}
+
+		if(parent.left){
+			parent.left.depthFirstLog(cb, parent.right);
+		}
 
 	};
 
